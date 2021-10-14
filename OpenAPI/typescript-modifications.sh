d84f9a2e9a5163ea481359114eb2a0c916e239b5
Kyle Scheuing <kyle@kylescheuing.com>
2021-10-14 04:11:33 -0400

    Add typescript sdk sourcemaps

--- OpenAPI/typescript-modifications.sh
+++ OpenAPI/typescript-modifications.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-EXISTS=$(grep "export class ResponseError " huokanclient-ts/src/runtime.ts)
-
-if [[ -z $EXISTS ]]; then
+if [[ -z $(grep "export class ResponseError " huokanclient-ts/src/runtime.ts) ]]; then
     sed -i -e "s/throw response;/throw new ResponseError(response, 'Response returned an error code');/g" huokanclient-ts/src/runtime.ts
     cat << EndOfCode >> huokanclient-ts/src/runtime.ts
 
@@ -15,4 +13,11 @@ EndOfCode
     echo "Added ResponseError."
 else
     echo "ResponseError already exists so no changes were made."
+fi
+
+if [[ -z $(grep '"sourceMap"' huokanclient-ts/tsconfig.json) ]]; then
+    sed -i -e 's/"compilerOptions": {/"compilerOptions": {\n    "sourceMap": true,/g' huokanclient-ts/tsconfig.json
+    echo "Added sourcemaps."
+else
+    echo "Sourcemaps already exist so no changes were made."
 fi
