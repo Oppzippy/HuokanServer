f6bd966d0c2ed0d261985f1a08e8886c16f3fda4
Kyle Scheuing <kyle@kylescheuing.com>
2022-01-25 02:51:54 -0500

    Add pagination to deposit api

---
+++ HuokanServer.EndToEndTests/Endpoints/Guilds/Deposits/DepositPaginationTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using HuokanServer.DataAccess.Repository.DepositRepository;
+using HuokanServer.EndToEndTests.TestBases;
+using HuokanServer.Web.Models;
+using Xunit;
+
+namespace HuokanServer.EndToEndTests.Endpoints.Guilds.Deposits;
+
+public class PaginationTest : OrganizationUserTestBase
+{
+	[Fact]
+	public async Task TestDepositPagination()
+	{
+		var depositRepository =
+			new DepositRepository(ConnectionFactory, new DepositImportExecutorFactory(ConnectionFactory));
+
+		var deposits = new List<Deposit>();
+		for (int i = 0; i < 55; i++)
+		{
+			deposits.Add(new Deposit()
+			{
+				CharacterName = $"Name{i}",
+				CharacterRealm = $"Realm",
+				DepositInCopper = i,
+				GuildBankCopper = i,
+			});
+		}
+
+		await depositRepository.Import(Organization.Id, Guild.Id, User.Id, deposits);
+		var page1 = await HttpClient.GetFromJsonAsync<DepositCollectionModel>(
+			$"{BaseUrl}/organizations/{Organization.Id}/guilds/{Guild.Id}/deposits");
+		Assert.Equal(50, page1.Deposits.Count);
+
+		Guid lastOnPage1 = page1.Deposits.Last().Id;
+		var page2 = await HttpClient.GetFromJsonAsync<DepositCollectionModel>(
+			$"{BaseUrl}/organizations/{Organization.Id}/guilds/{Guild.Id}/deposits?after={lastOnPage1}");
+		Assert.Equal(5, page2.Deposits.Count);
+
+		for (int i = 0; i < 50; i++)
+		{
+			Assert.Equal(i, page1.Deposits[i].DepositInCopper);
+		}
+
+		for (int i = 5; i < 5; i++)
+		{
+			Assert.Equal(50 + i, page2.Deposits[i].DepositInCopper);
+		}
+	}
+}
--- HuokanServer/Web/Controllers/v1/Organizations/Guilds/DepositsController.cs
+++ HuokanServer/Web/Controllers/v1/Organizations/Guilds/DepositsController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using HuokanServer.DataAccess.Repository.DepositRepository;
 using HuokanServer.DataAccess.Repository.UserPermissionRepository;
@@ -23,10 +24,37 @@ public class DepositsController : LoggedInControllerBase
 	[OrganizationPermissionAuthorizationFilterFactory(OrganizationPermission.MODERATOR)]
 	public async Task<DepositCollectionModel> GetDeposits(
 		[FromRoute(Name = "organizationId")] Guid organizationId,
-		[FromRoute(Name = "guildId")] Guid guildId
+		[FromRoute(Name = "guildId")] Guid guildId,
+		[FromQuery(Name = "after")] Guid? afterDepositId = null,
+		[FromQuery(Name = "limit")] [Range(1, 100)]
+		int limit = 50
 	)
 	{
-		List<BackedDeposit> deposits = await _depositRepository.GetDeposits(organizationId, guildId);
+		List<BackedDeposit> deposits;
+		if (afterDepositId != null)
+		{
+			deposits = await _depositRepository.GetDepositsAfter(organizationId, guildId, afterDepositId, limit);
+		}
+		else
+		{
+			deposits = await _depositRepository.GetDeposits(organizationId, guildId, limit);
+		}
+
 		return DepositCollectionModel.From(deposits);
+	}
+
+	[HttpGet]
+	[OrganizationPermissionAuthorizationFilterFactory(OrganizationPermission.MODERATOR)]
+	[Route("{depositId:guid}")]
+	public async Task<BackedDeposit> GetDeposit(
+		[FromRoute(Name = "organizationId")] Guid organizationId,
+		[FromRoute(Name = "guildId")] Guid guildId,
+		[FromRoute(Name = "depositId")] Guid depositId,
+		[FromQuery(Name = "offset")] [Range(int.MinValue, 0)]
+		int offset
+	)
+	{
+		BackedDeposit deposit = await _depositRepository.GetDeposit(organizationId, guildId, depositId, offset);
+		return deposit;
 	}
 }
