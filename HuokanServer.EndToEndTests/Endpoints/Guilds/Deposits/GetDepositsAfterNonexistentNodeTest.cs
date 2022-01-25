80266ef9b6f9dbb691513367710f3f5fbe2a9d2f
Kyle Scheuing <kyle@kylescheuing.com>
2022-01-25 02:59:06 -0500

    Add test for getting deposits after nonexistent deposit

---
+++ HuokanServer.EndToEndTests/Endpoints/Guilds/Deposits/GetDepositsAfterNonexistentNodeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using HuokanServer.EndToEndTests.TestBases;
+using HuokanServer.Web.Models;
+using Xunit;
+
+namespace HuokanServer.EndToEndTests.Endpoints.Guilds.Deposits;
+
+public class GetDepositsAfterNonexistentNodeTest : OrganizationUserTestBase
+{
+	[Fact]
+	public async Task TestGetDepositsAfterNonexistentNode()
+	{
+		var depositCollection = await HttpClient.GetFromJsonAsync<DepositCollectionModel>(
+			$"{BaseUrl}/organizations/{Organization.Id}/guilds/{Guild.Id}/deposits?after={Guid.Empty}");
+		Assert.Empty(depositCollection.Deposits);
+	}
+}
